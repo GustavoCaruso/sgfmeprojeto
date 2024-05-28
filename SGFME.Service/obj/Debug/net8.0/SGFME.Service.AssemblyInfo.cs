e4fcaf93e2e1986e49f9a8c950ba990150677d22
gustavocarus0_ <89716464+GustavoCaruso@users.noreply.github.com>
2024-05-28 11:49:24 -0300

    fff

--- SGFME.Service/obj/Debug/net8.0/SGFME.Service.AssemblyInfo.cs
+++ SGFME.Service/obj/Debug/net8.0/SGFME.Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SGFME.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+251cbf0566761bc50d367f70e0dccd158fe8d3ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+786179c0e177d85e1e5589ee3f5edc014cf8a147")]
 [assembly: System.Reflection.AssemblyProductAttribute("SGFME.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SGFME.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
