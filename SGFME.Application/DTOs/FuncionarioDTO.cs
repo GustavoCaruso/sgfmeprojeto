f86e7ff7f6bce70a40174d31d1dd5f2e083b4c05
gustavocarus0_ <89716464+GustavoCaruso@users.noreply.github.com>
2024-08-24 21:51:10 -0300

    Criação do funcionário dto

--- SGFME.Application/DTOs/FuncionarioDTO.cs
+++ SGFME.Application/DTOs/FuncionarioDTO.cs
@@ -2,5 +2,21 @@
 {
     public class FuncionarioDTO
     {
+        public long id { get; set; }
+        public string nomeCompleto { get; set; }
+        public DateTime dataNascimento { get; set; }
+        public DateTime dataCadastro { get; set; }
+        public string nomeMae { get; set; }
+        public string rgNumero { get; set; }
+        public DateTime rgDataEmissao { get; set; }
+        public string rgOrgaoExpedidor { get; set; }
+        public string rgUfEmissao { get; set; }
+        public string cnsNumero { get; set; }
+        public string cpfNumero { get; set; }
+        public string nomeConjuge { get; set; }
+        public string naturalidadeCidade { get; set; }
+        public string naturalidadeUf { get; set; }
+        public string? crfUf { get; set; }
+        public string? crf { get; set; }
     }
 }
